7caac5eb471b902b747858ce6770edab6fb09bd9
massona <albert.masson1@gmail.com>
2019-09-26 17:06:19 +0200

    commit includes

--- src/interval.test.js
+++ src/interval.test.js
@@ -3,6 +3,7 @@ const Interval = require('./interval');
     var interval1 = new Interval(1,4);
     var interval2 = new Interval(5,9);
     var interval3 = new Interval(3,7);
+    var interval4 = new Interval(6,7)
 
 describe('overlaps', function () {
 
@@ -12,5 +13,16 @@ describe('overlaps', function () {
 
     test('Test overlaps de (1,4) et (3,7) => true', () => {
         expect(interval1.overlaps(interval3)).toBe(true);
+    });
+});
+
+describe('includes', function () {
+
+    test('Test includes de (5,9) et (6,7) => true', () => {
+        expect(interval2.includes(interval4)).toBe(true);
+    });
+
+    test('Test includes de (1,4) et (3,7) => false', () => {
+        expect(interval2.includes(interval3)).toBe(false);
     });
 });
