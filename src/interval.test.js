ba76eec75ee17651ab1ece06a2cfec585d311aa0
massona <albert.masson1@gmail.com>
2019-09-26 17:44:46 +0200

    commit union

--- src/interval.test.js
+++ src/interval.test.js
@@ -3,7 +3,7 @@ const Interval = require('./interval');
     var interval1 = new Interval(1,4);
     var interval2 = new Interval(5,9);
     var interval3 = new Interval(3,7);
-    var interval4 = new Interval(6,7)
+    var interval4 = new Interval(6,7);
 
 describe('overlaps', function () {
 
@@ -25,4 +25,21 @@ describe('includes', function () {
     test('Test includes de (1,4) et (3,7) => false', () => {
         expect(interval2.includes(interval3)).toBe(false);
     });
+});
+
+
+describe('union', function () {
+    var interval = new Interval(4,7);
+    test.each([
+        [new Interval(5,6), [new Interval(4,7)]],
+        [new Interval(10,14), [new Interval(4,7), new Interval(10,14)]],
+        [new Interval(0,2), [new Interval(4,7), new Interval(0,2)]],
+        [new Interval(1,3), [new Interval(4,7), new Interval(1,3)]],
+        
+    ])(
+        'union %s %s',
+        (n, expected) => {
+            expect(interval.union(n)).toStrictEqual(expected);
+        }
+    );
 });
