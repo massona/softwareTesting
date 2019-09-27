7b054585a913751d0fea23b9fe0bfab929b30f61
massona <albert.masson1@gmail.com>
2019-09-27 11:53:56 +0200

    mutation

---
+++ stryker.conf.js
@@ -0,0 +1,16 @@
+
+      module.exports = function(config){
+        config.set(
+          {
+  "mutator": "javascript",
+  "packageManager": "npm",
+  "reporters": [
+    "clear-text",
+    "progress"
+  ],
+  "testRunner": "jest",
+  "transpilers": [],
+  "coverageAnalysis": "off"
+}
+        );
+      }
